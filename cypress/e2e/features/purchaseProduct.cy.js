3979a791c194c8873c720beae580ba00bfdbae6b
Silvia A. Rodriguez <silviarodriguezalonso70@gmail.com>
2024-09-20 17:42:05 -0600

    Se agrego correccion del assert

--- cypress/e2e/features/purchaseProduct.cy.js
+++ cypress/e2e/features/purchaseProduct.cy.js
@@ -10,14 +10,14 @@ describe("purchse Productos", () => {
     const cart = new cartPlaceOrderPage ()
     cart.selectplaceOrder()
     
-    cy.get("#name").type("Silvia Rodriguez")
+    cy.get("#name").type("Silvia")
     cy.get("#country").type("Mexico")
     cy.get("#city").type("Acuña")
     cy.get("#card").type("0123456789")
     cy.get("#month").type("09")
     cy.get("#year").type("30")
     cy.get("button[onclick='purchaseOrder()']").click()
-    cy.get('body > div:nth-child(20) > h2:nth-child(6)').should('be.visible').should('have.attr', 'body > div:nth-child(20) > h2:nth-child(6)', 'Thank you for your purchase!')
+    cy.contains('Thank you for your purchase!').should('be.visible')
   
     })
 
